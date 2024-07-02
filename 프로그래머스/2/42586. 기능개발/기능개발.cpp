de601431c6c0582cae6c5f7a0012c9727f4050df
Yoon-HB <93539648+Yoon-HB@users.noreply.github.com>
2024-07-02 11:22:35 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue <int> q, st;
+    
+    //남은 일 수 구하기
+    for(int i =0; i < progresses.size(); i++){
+        int n = 100 - progresses[i];
+        int div = n/speeds[i];
+        if(n%speeds[i] != 0){
+            div += 1;
+        }
+        q.push(div);
+        cout << q.back() << " ";
+    }
+    
+//     int cnt = 1;
+//     while(!q.empty()){
+        
+//         int s = q.front();
+//         q.pop();
+//         if(q.front() <= s){
+//             cnt++;
+//             q.pop();
+//         }
+//         else{
+//             answer.push_back(cnt);
+//             cnt = 1;
+//         }
+//     }
+//     answer.push_back(cnt);
+    
+    //스택이 비어있거나 큐에 있는 값이 크면 st.push
+    while(!q.empty()){
+        if(st.empty()){
+            st.push(q.front());
+            q.pop();
+        }
+        else if(q.front() > st.front()){
+            answer.push_back(st.size());
+            while( !st.empty() ) {
+                st.pop();
+            }
+                
+        }else if(q.front() <= st.front()){
+            st.push(q.front());
+            q.pop();
+        }
+        
+    }
+    answer.push_back(st.size());
+    //st.front()보다 q.front()가 크면 스택의 사이즈를 answer에 삽닙
+    //st.front()보다 작으면 st.push
+    
+    return answer;
+}
