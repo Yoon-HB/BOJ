25c6bf7071d5b18453d474839e29bf8c5a55404d
Yoon-HB <93539648+Yoon-HB@users.noreply.github.com>
2024-10-07 16:23:46 +0900

    [level 2] Title: 방문 길이, Time: 0.15 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,49 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//답안 참조
 int solution(string dirs) {
     int answer = 0;
-    map<pair<pair<int, int>, pair<int, int>>, bool> visited;
-        
+    
+    bool visited[11][11] = { false };
+    
+    // 출발 정점은 문제에선 (0, 0)긴한데 각 좌표를 배열로 다루기 위해 (5, 5)로 치환함. 좌상단이 배열 인덱스로 [0][0]가 되게끔 하기 위하여.
     int curX = 5;
     int curY = 5;
-   
     
-    for(int i = 0; i < dirs.length(); ++i){
+    visited[curX][curY] = true; // 출발지는 미리 방문 체크
+    
+    for(int i = 0; i < dirs.length(); i++){
         int startX = curX; // 현재 간선에서의 출발 정점 X좌표
         int startY = curY; // 현재 간선에서의 출발 정점 Y좌표
         
         /* 이동 */
         if (dirs[i] == 'U'){
-            if (curX - 1 < 0) // 무시
+            if (curX - 1 < 0)
                 continue;
             curX--;
         }
         if (dirs[i] == 'D'){
-            if (curX + 1 > 10) // 무시
+            if (curX + 1 > 10)
                 continue;
             curX++;
         }
         if (dirs[i] == 'L'){
-            if (curY - 1 < 0) // 무시
+            if (curY - 1 < 0)
                 continue;
             curY--;
         }
         if (dirs[i] == 'R'){
-            if (curY + 1 > 10) // 무시
+            if (curY + 1 > 10)
                 continue;
             curY++;
         }
         
         // 출발정점, 도착정점 둘 다 방문한적 있다면 무시    
-        if (visited[make_pair(make_pair(startX, startY), make_pair(curX, curY))] == true) continue;
+        if (visited[startX][startY] == true && visited[curX][curY] == true) continue;
         else{
-            visited[make_pair(make_pair(startX, startY), make_pair(curX, curY))] = true; // 방문 체크
-            visited[make_pair(make_pair(curX, curY), make_pair(startX, startY))] = true; // 역방향도 체크
-            
+            visited[curX][curY] = true; // 방문 체크
+            cout<<  curX << "," << curY << "\n";
             answer++; // answer 증가
-        }      
+            
+        }
+        
     }
     return answer;
 }
