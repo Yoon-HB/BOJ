9dfdaefe70a45a264fd35e86268d3bfbddf15073
Yoon-HB <93539648+Yoon-HB@users.noreply.github.com>
2024-10-31 15:45:27 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[100005];
+int main() {
+
+	int n;
+	cin >> n;
+
+	d[1] = 1;
+	d[2] = 3;
+
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + d[i - 2] * 2) % 10007;
+	}
+	cout << d[n];
+
+	return 0;
+}
