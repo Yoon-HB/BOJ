5f4d38226eb0327f6e5a5e20572cd8445308e028
윤해빈 <93539648+Yoon-HB@users.noreply.github.com>
2025-09-24 16:37:12 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+tuple<int, int, int> edge[100002]; //간선 기준,,,
+vector<int> p(100002, -1);
+
+int find(int x) {
+	if (p[x] < 0) return x;
+	return p[x] = find(p[x]);
+}
+
+bool is_diff_group(int u, int v) {
+	u = find(u);
+	v = find(v);
+
+	if (u == v) return 0;
+	if (p[u] > p[v]) swap(u, v);
+
+	if (p[u] == p[v]) p[u]--;
+	p[v] = u;
+
+	return true;
+
+}
+
+int main() {
+
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+
+	cin >> N;
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		edge[i] = { c, a, b };
+	}
+	sort(edge, edge + M);
+
+	int cnt = 0;
+	int ans= 0; 
+
+	for (int i = 0; i < M; i++) {
+		int cost, a, b;
+		tie(cost, a, b) = edge[i];
+		if (!is_diff_group(a, b)) continue;
+		cnt++;
+		ans += cost;
+
+		if (cnt == N - 1) break;
+	}
+
+	cout << ans;
+	return 0;
+
+}
