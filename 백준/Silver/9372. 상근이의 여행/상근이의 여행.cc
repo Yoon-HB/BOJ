a378296b8b55625782bc8dfa7229a86043b70a6b
윤해빈 <93539648+Yoon-HB@users.noreply.github.com>
2025-09-24 19:46:52 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+
+		int N, M;
+		cin >> N >> M;
+
+		for (int i = 0; i < M; i++) {
+			int a, b;
+			cin >> a >> b;
+			
+		}
+		
+		cout << N-1 << "\n";
+	}
+
+	return 0;
+}
