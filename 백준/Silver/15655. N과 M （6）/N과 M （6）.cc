f043a522e076391dbfbccce2a0016b52d7209188
윤해빈 <93539648+Yoon-HB@users.noreply.github.com>
2025-10-07 16:58:28 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+
+int arr[8];
+vector<int> res;
+
+void backTracking(int cnt, int st) {
+    if (cnt == M) { // M개 다 골랐다면
+        for (int i = 0; i < M; i++) {
+            cout << res[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = st; i < N; i++) {
+        res.push_back(arr[i]);
+        backTracking(cnt + 1, i + 1 ); // 다음에는 i+1부터
+        res.pop_back();
+    }
+}
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+
+    backTracking(0, 0);
+
+
+    return 0;
+}
